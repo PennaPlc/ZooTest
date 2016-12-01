182cfaa7472ec0834c7f517097dd32a8bfd704bc
Justyna <justyna.heinbockel@penna.com>
2016-12-01 17:36:27 +0000

    Basic project setup

---
+++ PennaZoo/Controllers/AnimalsController.cs
@@ -0,0 +1,30 @@
+﻿using PennaZoo.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PennaZoo.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private ZooDatabaseEntities db = new ZooDatabaseEntities();
+
+        // GET: Animals
+        public ActionResult Index()
+        {
+            var animals = db.Animals
+                .Include(a => a.Species);
+            return View(animals.ToList());
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PennaZoo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PennaZoo.MvcApplication" Language="C#" %>
